874a831de82ddad70a56199bd65aadbdc195414b
Yunlong Xu <vincent881229@gmail.com>
2015-11-23 13:20:13 -0800

    remove Old Vacrooms folder

--- Vacrooms/rate.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `wcd_rate` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_post` int(11) NOT NULL,
-  `ip` varchar(40) NOT NULL,
-  `rate` int(11) NOT NULL,
-  `dt_rated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-)
